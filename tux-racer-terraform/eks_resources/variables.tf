362f60de4577edee25ac53c3bd446de571d6b305
Travis Dumont <tdumont97566@gmail.com>
2025-04-30 21:03:11 -0500

    test hud change

--- tux-racer-terraform/eks_resources/variables.tf
+++ tux-racer-terraform/eks_resources/variables.tf
@@ -17,5 +17,5 @@ variable "tux_racer_version" {
 variable "image_name" {
   description = "the name of the image container in ecr"
   type        = string
-  default     = "tux-racer-ts"
+  default     = "tux-racer-js"
 }
